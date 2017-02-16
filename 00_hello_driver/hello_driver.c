58d29645bc0912c8f4cbb0b4dd753dd56a74e7bc
Allen Mamaril <allen.mamaril@gmail.com>
2017-02-16 11:30:14 -0800

    Add module license type and clean code

--- 00_hello_driver/hello_driver.c
+++ 00_hello_driver/hello_driver.c
@@ -2,20 +2,19 @@
  *  hello_driver.c - The simplest kernel module.
  */
 #include <linux/init.h>
-#include <linux/module.h>	/* Needed by all modules */
-#include <linux/kernel.h>	/* Needed for KERN_INFO */
+#include <linux/module.h>	// Needed by all modules
+#include <linux/kernel.h>	// Needed for KERN_INFO
 
 MODULE_AUTHOR("Allen Mamaril");      // The author -- visible when you use modinfo
 MODULE_DESCRIPTION("A simple Linux driver for the BBB.");  // The description -- see modinfo
-MODULE_VERSION("0.1");              // The version of the modul
+MODULE_VERSION("0.1");              // The version of the module
+MODULE_LICENSE("gpl");
 
 int __init km_init(void)
 {
-	printk(KERN_INFO "Hello world 1.\n");
+	printk(KERN_INFO "Hello world from Kernel Land!\n");
 
-	/* 
-	 * A non 0 return means init_module failed; module can't be loaded. 
-	 */
+	// A non 0 return means init_module failed; module can't be loaded.
 	return 0;
 }
 
